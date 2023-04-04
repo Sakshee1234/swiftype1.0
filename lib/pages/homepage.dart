e93cfb748406cfd032df042aec8020d8d0d9cec4
aakritip11 <118073835+aakritip11@users.noreply.github.com>
2023-04-04 11:49:56 +0530

    ML corresponding code in Flutter

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,19 +1,28 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  TextEditingController typing = TextEditingController();
-  void signUserOut() async {
-    await FirebaseAuth.instance.signOut();
+  final TextEditingController typing = TextEditingController();
+  List<String> _autocompleteSuggestions = [];
+
+  Future<List> _getAutocompleteSuggestions(String query) async {
+    String apiUrl = "https://api.datamuse.com/words?max=5&sp=$query*";
+    var response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((word) => word['word']).toList();
+    } else {
+      throw Exception('Failed to load suggestions');
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -29,20 +38,40 @@ class HomePageState extends State<HomePage> {
                   height: 50,
                 ),
                 
-                SizedBox(
-                  height: 25,
-                ),
+                // SizedBox(
+                //   height: 25,
+                // ),
                 SizedBox(
                     width: 350, // <-- TextField width
-                    height: 500, // <-- TextField height
+                    //height: 500, // <-- TextField height
                     child: Container(
-                      child: 
-                        TextField(
-                          maxLines: null,
-                          expands: true,
-                          decoration: InputDecoration( filled:true,fillColor: Colors.white,hintText: 'Start Typing...'),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TypeAheadField(
+                        textFieldConfiguration: TextFieldConfiguration(
+                          //maxLines: 10,
                           controller: typing,
+                          decoration: InputDecoration(
+                            hintText: 'Start Typing...',
+                            border: OutlineInputBorder(),
+                          ),
                         ),
+                        suggestionsCallback: (pattern) async {
+                          if (pattern.isEmpty) {
+                            return [];
+                          }
+                          return await _getAutocompleteSuggestions(pattern);
+                        },
+                        itemBuilder: (context, suggestion) {
+                          return ListTile(title: Text(suggestion));
+                        },
+                        onSuggestionSelected: (suggestion) {
+                          typing.text = suggestion;
+                        },
+                      ),
                     ),
                     
                   ),
